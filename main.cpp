6f08e64d5516c35da1662ec42891c23894f29b86
amlandy <amlandy@umich.edu>
2018-12-26 00:24:24 -0500

    Fixed comment error

--- main.cpp
+++ main.cpp
@@ -13,17 +13,17 @@
 
 
 // main.cpp just runs test 1 as an example
-//int main() {
-//    
-//    std::ifstream p1_stream;
-//    p1_stream.open("/Users/alandy/Documents/Sudoku1/Sudoku/Test_Files/test-1-input.txt");
-//    
-//    Solver tester(9, p1_stream);
-//    tester.solve_puzzle(std::cout);
-//    
-//    p1_stream.close();
-//
-//    return 0;
-//}
+int main() {
+    
+    std::ifstream p1_stream;
+    p1_stream.open("/Users/alandy/Documents/Sudoku1/Sudoku/Test_Files/test-1-input.txt");
+    
+    Solver tester(9, p1_stream);
+    tester.solve_puzzle(std::cout);
+    
+    p1_stream.close();
+
+    return 0;
+}
 
 
